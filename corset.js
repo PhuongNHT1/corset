5d6a93c832a7703e33888d8ca9584f6bbbb7eea6
Joe Hitchens <joe@sleepless.com>
2013-10-02 14:33:12 -0700

    -

--- corset.js
+++ corset.js
@@ -18,20 +18,49 @@ function init() {
     chrome.browserAction.onClicked.addListener(showOptions);
 
 	function onHeadersReceivedHandler(info) {
-		var headers = info.responseHeaders;
-		headers.push("Access-Control-Allow-Origin", "*");
-		return { responseHeaders:headers };
+		log("onHeadersReceivedHandler: "+info.json());
+		var hdrs = info.responseHeaders;
+		/*hdrs.forEach(function(h) {
+			log("RSP: "+h.name+": "+h.value);
+		});*/
+		hdrs.push({name:"Access-Control-Allow-Origin", value:"*"});
+		return { responseHeaders:hdrs };
 	}
 
-	// remove listener
+	function onBeforeSendHeadersHandler(info) {
+		var hdrs = info.requestHeaders;
+		var url = "http://"+host;
+		log("url "+url);
+		var clipped = info.url.substring(0, url.length);
+		log("clp "+clipped);
+		if(url == clipped) {
+			hdrs.forEach(function(hdr) {
+				if(hdr.name.toLowerCase() == "origin") {
+					hdr.value = url;
+					log("REQ: "+hdr.name + ": "+hdr.value);
+				}
+				//log("REQ: "+hdr.name + ": "+hdr.value);
+			});
+			//hdrs.push({name:"Origin",value: "http://"+host+"/"}); // XXX doubt this works
+		}
+		return {requestHeaders: hdrs};
+	}
+
+	// remove listeners
     if (chrome.webRequest.onHeadersReceived.hasListener(onHeadersReceivedHandler)) {
         chrome.webRequest.onHeadersReceived.removeListener(onHeadersReceivedHandler)
     }
+    if (chrome.webRequest.onHeadersReceived.hasListener(onBeforeSendHeadersHandler)) {
+        chrome.webRequest.onHeadersReceived.removeListener(onBeforeSendHeadersHandler)
+    }
 	if(host) {
-		// re-add listener
-		var url = "http://" + url + "/*";
+		// re-add listeners
+		var url = "http://" + host + "/*";
 		chrome.webRequest.onHeadersReceived.addListener(
 			onHeadersReceivedHandler, { urls:[url] }, ["blocking", "responseHeaders"]
+		);
+		chrome.webRequest.onBeforeSendHeaders.addListener(
+			onBeforeSendHeadersHandler, {urls:[url]}, ["blocking", "requestHeaders"]
 		);
 	}
 
